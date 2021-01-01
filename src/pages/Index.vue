6a652ff72fe2a3a7dab52cbbf416c7e0dcba49a0
Alain Ramirez Cabrejas <alain.ramirez@get.tur.cu>
2021-01-01 15:45:33 -0500

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+    <q-btn color="primary" label="scan" @click="scan"/>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  methods: {
+    scan () {
+      cordova.plugins.barcodeScanner.scan(
+        function (result) {
+          alert('We got a barcode\n' +
+            'Result: ' + result.text + '\n' +
+            'Format: ' + result.format + '\n' +
+            'Cancelled: ' + result.cancelled)
+        },
+        function (error) {
+          alert('Scanning failed: ' + error)
+        },
+        {
+          preferFrontCamera: true, // iOS and Android
+          showFlipCameraButton: true, // iOS and Android
+          showTorchButton: true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt: 'Place a barcode inside the scan area', // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats: 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
+          orientation: 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations: true, // iOS
+          disableSuccessBeep: false // iOS and Android
+        }
+      )
+    }
+  }
+}
+</script>
